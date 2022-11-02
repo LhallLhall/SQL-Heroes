abff74a7c29faf9279fafe5fe331e1a9a8371bf8
Logan Hall <logan.hall@student.awesomeinc.org>
2022-11-02 21:16:35 +0000

    added some pseudocode and some functions

--- pseudocode.md
+++ pseudocode.md
@@ -1,7 +1,15 @@
 # FUNCTIONS
+- select_all()
+    - loops through all items in hero and returns each heroes name
+- set main character
+    - create an input() that asks for their super hero name
+    - create an input() that asks for their about_me
+    - create an input() that asks for their bio
+    - query (use sql)
+- 
 
 # THE PROCESS
-
+- 
 # QUERIES MAYBE?????
 
 # QUESTIONS
--- src/database/connection.py
+++ src/database/connection.py
@@ -31,3 +31,28 @@ def execute_query(query, params=None):
     
 create_connection('postgres', "postgres", "postgres")
 
+# DONT EDIT ABOVE THIS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# def select_all():
+#     query = """
+#         SELECT * FROM heroes
+#     """
+
+#     list_of_heroes = execute_query(query).fetchall()
+#     # print(list_of_heroes)
+#     for record in list_of_heroes:
+#         print(record[1])
+
+# select_all()
+
+def set_main_character():
+    name = input('Hi SuperHero! What should I call you?: ')
+    about = input('I like that name! Tell me a little about your self!: ')
+    bio = input("Oh wow! You are a very interesting person! One last question. How did you get your powers?: ")
+    params = (name, about, bio)
+    query = """
+            INSERT INTO heroes (name, about_me, biography) VALUES (%s, %s, %s)
+            """
+    execute_query(query, params)
+
+set_main_character()
